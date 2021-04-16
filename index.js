14a39f6c28851ba0514fa6f083a16d42cb65ebdf
Aforbes918 <andre.forbes@ontariotechu.net>
2021-04-15 22:37:31 -0400

    Started Login

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+express = require('express');
+let session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
 let app = express();
 
 app.set('views', __dirname + '/views');
@@ -6,6 +8,26 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
+app.use(session({
+    genid: () => uuidv4(),
+    resave: false,
+    saveUninitialized: false,
+    secret: 'passphrase',
+
+}));
+
+//Process logining
+app.post('/processLogin',function(request,response){
+    console.log(request.body);
+    request.session.username = request.body.username;
+    request.session.password = request.body.passsword;
+    response.send('Login Successful');
+});
+
+app.get('/logout', function(request,response){
+    request.session.username = '';
+    request.session.passsword = '';
+});
 
 app.set('port', process.env.PORT || 3000); 
 
